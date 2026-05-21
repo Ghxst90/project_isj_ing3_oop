cc65ba7708afbd6acdc910ffb92951eaeb971729
Ghxst90 <aroungstephane@gmail.com>
2026-05-22 00:48:55 +0100

    :feat: module 5 - menu interactif sans dependances externes

--- src/main.py
+++ src/main.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Ajoute src/ au chemin Python pour que les imports fonctionnent
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
 from equipements import Routeur, Switch, Serveur, Firewall, PointAccesWifi, Terminal
 from topologie   import Topologie, Lien
 from paquets     import Paquet, Simulateur
